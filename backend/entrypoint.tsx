c161103fbf2f5f21c6dc308d1579d20fcfcf29a7
Jonas Strehle <jonas@strehles.info>
2024-04-08 11:20:55 +0200

    Initial project setup

---
+++ backend/entrypoint.tsx
@@ -0,0 +1,11 @@
+/**
+ * Backend entrypoint:
+ * This module provides a default export that defines the UI that is returned from the backend
+ * when a page is visited
+ */
+export default {
+	'/': null,
+	'/iframe': <p>
+		Hello world from backend-1
+	</p>
+}
